3b288e300f89dbbdecfeeea7b1393d7ce4e98536
SaerusTierialis <me12399@gmail.com>
2020-11-12 21:29:27 -0500

    move unique autodata out of default container file

--- Containers/AutoData.cs
+++ Containers/AutoData.cs
@@ -140,16 +140,4 @@ namespace EAC2.Containers
         protected virtual void OnChange() { }
         protected virtual void OnChangeLocal() { }
     }
-
-    public class CharacterLevel : AutoData<uint>
-    {
-        public CharacterLevel(PlayerModule parent, byte id, uint value_initial, bool syncs = false, bool resets = false) : base(parent, id, value_initial, syncs, resets)
-        {
-        }
-
-        protected override void OnChange()
-        {
-            Systems.XPRewards.Rewards.UpdateXPMultiplier();
-        }
-    }
 }
