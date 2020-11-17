db15fce2b84368e5b4e5be3d1f7f347b88a22b5b
SaerusTierialis <me12399@gmail.com>
2020-11-17 16:41:00 -0500

    Create MainUI.cs

---
+++ UI/MainUI.cs
@@ -0,0 +1,35 @@
+﻿using ACE.Containers;
+using ACE.UI.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.UI.Elements;
+
+namespace ACE.UI
+{
+    public class MainUI : UIModule
+    {
+        private const float WIDTH = 600f;
+        private const float HEIGHT = 600f;
+
+        private Draggable _main;
+
+        public MainUI(UIData parent, bool visible = false) : base(parent, visible)
+        {
+            _main = new Draggable();
+            _main.Resize(WIDTH, HEIGHT);
+            Append(_main);
+
+            ACEPanel panel = new ACEPanel();
+            panel.Resize(WIDTH, HEIGHT);
+            _main.Append(panel);
+        }
+
+        public override void OnInventoryStateChange(bool state)
+        {
+            Hide();
+        }
+    }
+}
