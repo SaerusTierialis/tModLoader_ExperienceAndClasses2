9156c55758b77e7b7cbc9676486d8f5fd794a658
SaerusTierialis <me12399@gmail.com>
2020-11-18 18:29:53 -0500

    use new ui elements

--- UI/MainUI.cs
+++ UI/MainUI.cs
@@ -1,6 +1,9 @@
 ﻿using ACE.Containers;
 using ACE.UI.Elements;
 using ACE.Utilities;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
 
 namespace ACE.UI
 {
@@ -8,8 +11,11 @@ namespace ACE.UI
     {
         private const float WIDTH = 600f;
         private const float HEIGHT = 600f;
+        private const float HEIGHT_MAIN_TITLE = 30f;
+        private const float PADDING = 5f;
 
         private Draggable _main;
+        private TitledPanel _main_panel;
 
         public MainUI(UIData parent) : base(parent) { }
 
@@ -19,9 +25,9 @@ namespace ACE.UI
             _main.Resize(WIDTH, HEIGHT);
             Append(_main);
 
-            ACEPanel panel = new ACEPanel();
-            panel.Resize(WIDTH, HEIGHT);
-            _main.Append(panel);
+            _main_panel = new TitledPanel(HEIGHT_MAIN_TITLE);
+            _main_panel.Resize(WIDTH, HEIGHT);
+            _main.Append(_main_panel);
         }
 
         public override void OnInventoryStateChange()
